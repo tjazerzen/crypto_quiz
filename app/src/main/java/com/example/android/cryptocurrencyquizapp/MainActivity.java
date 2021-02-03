8e6ae6e4e7a0b2328e0269c7645a5f5c2dc574a3
Tjaž Eržen <erzen.tjaz@gmail.com>
2021-02-03 14:30:34 +0100

    The starting total value changed back to 0.

--- app/src/main/java/com/example/android/cryptocurrencyquizapp/MainActivity.java
+++ app/src/main/java/com/example/android/cryptocurrencyquizapp/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     /*
      * totalScore will track the total score of the quiz.
      */
-    int totalScore = 10;
+    int totalScore = 0;
 
     /*
      * Functions that tell whether the answers are correct or not.
